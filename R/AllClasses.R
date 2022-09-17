6f0dd238b6f37c3c7dbd30b9bbecbaaae5889fdf
Sourish <54475400+sourish279@users.noreply.github.com>
2022-09-17 14:15:49 +1000

    Delete R directory

--- R/AllClasses.R
+++
@@ -1,9 +0,0 @@
-#' @export
-#' @rdname SegmentedCells
-#' @importClassesFrom S4Vectors DataFrame
-setClass("SegmentedCells", contains = "DataFrame")
-
-#' @export
-#' @aliases SpicyResults,list,ANY-method
-#' @rdname spicy
-setClass("SpicyResults", contains = "list")
