df6c08fdb4f076e79d6e5fada688c4b1ee822ffa
Farhan Ameen <farhana@maths.usyd.edu.au>
2022-10-06 00:36:00 +1100

    Compressed data.
    Former-commit-id: d321decf642f446b9b2dd90f32c010a7b2c04834

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' Head and Neck cancer IMC data 
+#'
+#'
+#' This is a imaging mass cytometry dataset from Ferguson et al 2022. The object contains 29 images.
+#'
+#'
+#' @format headData a SingleCellExperiment object
+#' @aliases 
+#' headSCE
+"headSCE"
+
