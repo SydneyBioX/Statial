a5613826e8291d3c9610027f988258a2a64a6b12
Farhan Ameen <farhana@maths.usyd.edu.au>
2022-10-06 09:55:56 +1100

    Fixed ClassENV issue

--- vignettes/Konditional.R
+++ vignettes/Konditional.R
@@ -12,8 +12,7 @@ library(BiocStyle)
 #Loading required packages
 library(Statial)
 library(tidyverse)
-library(spatstat)
-library(EBImage)
+library(plotly)
 
 theme_set(theme_classic())
 
@@ -24,10 +23,12 @@ theme_set(theme_classic())
 
 ## -----------------------------------------------------------------------------
 # Load head and neck data
-sce = readRDS("../data/Head and Neck/sce.RDS")
+#data("headSCE")
+load("../data/headSCE.rda")
+
 
 # Examine all cell types in image
-unique(sce$cellType)
+unique(headSCE$cellType)
 
 # Set up cell populations
 tumour = c("SC1", "SC2", "SC3", "SC4", "SC5", "SC6", "SC7")
@@ -47,17 +48,24 @@ all = c(tumour, tissue, immune, "Undefined")
 
 
 ## -----------------------------------------------------------------------------
-konditionalResult = Konditional(
-    imageData = sce,
+CD4_Konditional = Konditional(
+    imageData = headSCE,
     r = 50,
     from = "TC_CD4",
-    to = "SC1",
+    to = "SC5",
     parent = immune,
     cores = 40
 )
 
-konditionalResult
+CD4_Konditional
 
+## -----------------------------------------------------------------------------
+plot = ggplot(CD4_Konditional, aes(x = original, y = konditional, col = imageID)) + 
+    geom_point() +
+    geom_hline(yintercept = 0, col = "red", linetype = "dashed") +
+    geom_vline(xintercept = 0, col = "red", linetype = "dashed")
+
+ggplotly(plot)
 
 ## -----------------------------------------------------------------------------
 # Get all relationships between cell types and their parents
@@ -66,11 +74,42 @@ parentDf
 
 ## -----------------------------------------------------------------------------
 # Selecting Image 1 as an example
-image_1 = sce %>% colData %>% data.frame() %>% filter(imageID == "1")
+image_1 = headSCE %>% colData %>% data.frame() %>% filter(imageID == "1")
 
-konditionalResult = Konditional(image_1, 
+
+image1_Konditional = Konditional(image_1, 
                                 parentDf = parentDf,
                                 r = 50,
                                 cores = 40)
-konditionalResult
+image1_Konditional
+
+## -----------------------------------------------------------------------------
+set.seed(10)
+
+#simulating images
+simulation = simulateCompartment(includeTissue = FALSE)
+
+#selecting image where a significant conditional relationship exists
+conditionalImage = simulation$sig
+
+#plotting image
+ggplot(conditionalImage, aes(x = x, y = y, col = cellType)) +
+    geom_point()
+
+
+## -----------------------------------------------------------------------------
+rsDf = rsCurve(
+    conditionalImage,
+    from = "cd8_t_cells",
+    to = "tumour_cells",
+    parent = c("cd8_t_cells", "t_cells"),
+    rs = seq(0.01, 0.15, 0.01),
+    cores = 40
+)
+
+ggplotRs(rsDf)
+
+
+## -----------------------------------------------------------------------------
+sessionInfo()
 
