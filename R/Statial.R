a3d8bcc26cd338a0824b60132fad88416223e818
Alex Qin <alexq@maths.usyd.edu.au>
2023-08-01 16:57:47 +1000

    changed ..density.. to after_state(density)
    Former-commit-id: f9b7ee6f87d56aa68d657a2879a573201e802402
    Former-commit-id: ef0ef550d0af21115da9df7e6c962eb1b5e36dbf

--- R/Statial.R
+++ R/Statial.R
@@ -746,7 +746,7 @@ plotStateChanges <- function(cells,
      ggplot2::stat_density_2d(
       data = data[data$cellType == to, ],
       ggplot2::aes(
-        x = x, y = y, fill = ..density..
+        x = x, y = y, fill = after_stat(density)
       ),
       geom = "raster",
       contour = FALSE
