16ff5b3bae1afe3c7954129e73474694ce307fb6
alexq <alexq@maths.usyd.edu.au>
2023-08-15 12:22:29 +1000

    another typo

--- R/Statial.R
+++ R/Statial.R
@@ -794,7 +794,7 @@ plotStateChanges <- function(cells,
     ggplot2::theme_classic() +
     ggplot2::ggtitle("State Change Scatter Plot") +
     ggplot2::ylab(paste(marker, "expression")) +
-    ggplot2::xlab(paste(from "cell distance from ", to)) +
+    ggplot2::xlab(paste(from, "cell distance from ", to)) +
     ggplot2::ylim(-1, NA)
   
   
